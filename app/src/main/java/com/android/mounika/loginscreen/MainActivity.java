63155d0244fcd62c41eed664e958aeba8f3408df
mounika <mounikapathuri2017@gmail.com>
2017-07-02 16:15:37 +0100

    Initial commit

---
+++ app/src/main/java/com/android/mounika/loginscreen/MainActivity.java
@@ -0,0 +1,22 @@
+package com.android.mounika.loginscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+        menu.add("logout");
+        return true;
+    }
+}
